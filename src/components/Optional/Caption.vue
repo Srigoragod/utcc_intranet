72c972b4f35dc378230a2ebf4ed8d7a8cec878bb
Nuttey :) <59074749+Srigoragod@users.noreply.github.com>
2024-01-27 23:27:34 +0700

    Coption

--- src/components/Optional/Caption.vue
+++ src/components/Optional/Caption.vue
@@ -1,28 +1,51 @@
 <template>
   <div class="w-[18rem]">
-    <div class="overlay-container w-5/6 ">
-      <div class="centered text-white p-2">
-        <h4 class="pt-20 px-2">คนที่ไม่เคยทำผิด คือคนที่ไม่เคยทำอะไรเลย</h4>
-        <h6 class="block text-right pr-4">– ธีโอดอร์ รุสเวลต์ -</h6>
+    <div class="overlay-container w-5/6">
+      <div class="centered text-white p-2 w-full">
+        <h4 class="block pt-20 px-2" v-html="captionMsg"></h4>
+        <h6 class="block text-right p-4 " v-html="captionOwner"></h6>
       </div>
     </div>
-    <div class="flex justify-self-center float-right  items-center text-[18px] text-right pt-2 cursor-pointer hover:text-a-blue-0874D9">
-      <span class="pr-2 hover:underline hover:underline-offset-1"> คลิกเสนอ "แคปชั่นทํางาน"</span> 
+    <div
+      class="flex justify-self-center float-right items-center text-[18px] text-right pt-2 cursor-pointer hover:text-a-blue-0874D9"
+    >
+      <span class="pr-2 hover:underline hover:underline-offset-1">
+        คลิกเสนอ "แคปชั่นทํางาน"</span
+      >
       <font-awesome-icon :icon="['fas', 'arrow-right']" class="text-[14px]" />
     </div>
   </div>
 </template>
 
 <script>
-export default {
+import { ref, defineComponent, onMounted } from "vue";
+//data
+import captiondata from "../../data/captiondata.json";
+export default defineComponent({
   name: "CaptionForWork",
   setup(props) {
-    
-    return{
+    const data = ref(captiondata);
+    const captionMsg = ref("");
+    const captionOwner = ref("");
 
-    }
-  }
-};
+    const randomCaption = () => {
+      const randomIndex = Math.floor(Math.random() * data.value.length);
+ 
+      const randomCaption = data.value[randomIndex];
+
+      captionMsg.value = randomCaption.caption_title;
+      captionOwner.value = randomCaption.caption_owner
+    };
+    onMounted(() => {
+     randomCaption(); 
+    });
+    randomCaption(); 
+    return {
+      captionMsg,
+      captionOwner,
+    };
+  },
+});
 </script>
 
 <style lang="scss" scoped>

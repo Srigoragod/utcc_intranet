550c385d9fb3f76fa7164a31dede7ba4eac3f288
Nuttey :) <59074749+Srigoragod@users.noreply.github.com>
2024-02-08 22:08:21 +0700

    Fix new

--- src/layout/News.vue
+++ src/layout/News.vue
@@ -3,7 +3,7 @@
     <div class="py-2">
       <TextUnderline class="mt-8" :text="'ข่าวสาร'" :desc="'ข่าวสารประชาสัมพันธ์ภายนอก ระบบสารสนเทศสืบค้นหนังสือเวียน'" ></TextUnderline>
     </div>
-    <div class="is-desktop grid grid-cols-4 gap-4">
+    <div class="grid-news">
       <div data-aos="fade-right" class="item-img-news hover:cursor-not-allowed sm:hidden"></div>
       <a target="_blank" data-aos="fade-left" href="http://center.utcc/search_result_seminar.php" class="">
         <div class="item-news px-8 static hover:shadow-lg">
@@ -28,44 +28,14 @@
         <div class="item-news static hover:shadow-lg">
           <div class="centered-div">
             <!-- alt=""  -->
-            <img src="../assets/icon/3News.svg" class="pl-2 w-[70px] m-auto" />
+            <img src="../assets/icon/3News.svg" class="pl-2  m-auto" />
             <h5 class="text-title">ข้อเสนอบริการวิชาการ และงานวิจัย</h5>
             <p class="text-desc ">ขออภัย เว็บไซต์อยู๋ในระหว่างปรับปรุง</p>
           </div>
         </div>
       </span>
     </div>
-    <div class="is-mobile grid grid-cols-4 gap-4">
-      <a target="_blank" data-aos="fade-left" href="http://center.utcc/search_result_seminar.php" class="">
-        <div class="item-news px-8 static hover:shadow-lg">
-          <div class="centered-div">
-            <img src="../assets/icon/laptop.svg" alt="" class="pl-2 mx-auto" />
-            <h5 class="text-title ">อบรม/สัมมนา</h5>
-            <p class="text-desc ">หมวดเอกสารที่เกี่ยวช้องกับการอบรม /สัมมนา</p>
-          </div>
-        </div>
-      </a>
-      <a target="_blank" data-aos="fade-left" href="http://center.utcc/search_result_academic.php">
-        <div class="item-news static hover:shadow-lg">
-          <div class="centered-div">
-            <img src="../assets/icon/calendar.svg" alt="" class="pl-2 mx-auto" />
-            <h5 class="text-title">ประชุมวิชาการ /เสนอผลงานวิชาการ</h5>
-            <p class="text-desc ">หมวดเอกสารประชุมวิชาการ / เสนอผลงานทางวิชาการ</p>
-          </div>
-        </div>
-      </a>
-      <!--  href="http://department.utcc.ac.th/admin/index.php/2018-03-06-08-45-13"   target="_blank" -->
-      <span data-aos="fade-left" class="cursor-not-allowed opacity-50">
-        <div class="item-news static hover:shadow-lg">
-          <div class="centered-div">
-            <!-- alt=""  -->
-            <img src="../assets/icon/3News.svg" class="pl-2 w-[70px] m-auto" />
-            <h5 class="text-title">ข้อเสนอบริการวิชาการ และงานวิจัย</h5>
-            <p class="text-desc ">ขออภัย เว็บไซต์อยู๋ในระหว่างปรับปรุง</p>
-          </div>
-        </div>
-      </span>
-    </div>
+    
   </div>
 </template>
 
@@ -81,44 +51,21 @@ export default {
 
 <style lang="scss" scoped>
 @import "../style/base.scss";
-.is-desktop{
-  @include mobile {
-    display: none;
-  }
-}
 
-.is-mobile {
-  padding: 0 1rem;
-  @include tablet{
-    display: none;
-  }
-  @include mobile {
-    grid-template-columns: repeat(3, minmax(0, 1fr));
-    gap: 0.5rem;
-    .item-news {
-      padding: 0.125rem;
-      height: auto;
-      img {
-        height: 2rem;
-        padding: 0;
-      }
-    }
-    .text-title {
-      font-size: 1.125rem;
-      line-height: 1;
-      height: 3rem;
-      color: $a-blue-005BC0;
-      margin-bottom: 0.5rem;
-    }
-    .text-desc{
-      font-size: 1rem;
-      line-height: 1;
-      display: none;
-    }
-  }
-}
 .container-news {
-  cursor: pointer;
+  padding: 1rem;
+  .grid-news{
+    display: grid;
+    grid-template-columns: repeat(4,1fr);
+    grid-template-rows: auto;
+    gap: 1rem;
+
+    @include mobile {
+      grid-template-columns: repeat(2, minmax(0, 1fr));
+
+    }
+
+  }
 
   @include min-desktop{
     padding: 0 1rem;
@@ -136,7 +83,7 @@ export default {
 
   .item-img-news {
     width: 100%;
-    height: 210px;
+    height: 100%;
     position: relative;
     display: inline-block;
     overflow: hidden;
@@ -146,10 +93,13 @@ export default {
     background-repeat: no-repeat;
     background-size: cover;
     background-position: center;
-    @include mobile {
-     display: none;
 
-    }
+    // @include mobile {
+    //  display: none;
+
+    // }
+
+ 
   }
   .item-news {
     display: flex;
@@ -163,15 +113,30 @@ export default {
     @include mobile {
       height: auto;
       padding: .5rem 0;
+      // border: 1px solid transparent;
     }
     .centered-div {
       text-align: center;
       img {
-        margin-top: 0.5rem;
-        margin-bottom: 0.5rem;
+        margin: 0.5rem auto;
+        padding: 1rem;
+        @include mobile {
+   
+          // width: 80px;
+          // height:80px;
+          // background-color: red;
+        }
       }
       h5 {
         line-height: normal;
+        @include mobile {
+          font-size:  1rem;
+        }
+      }
+      .text-desc{
+        @include mobile {
+          display: none;
+        }
       }
     }
     &:hover {

77b2593a3ebc12735167e9cad7402762c31f2d76
Nuttey :) <59074749+Srigoragod@users.noreply.github.com>
2024-02-14 15:49:15 +0700

    fix menu mobile

--- src/components/Menu/MenuMobile.vue
+++ src/components/Menu/MenuMobile.vue
@@ -3,7 +3,7 @@
     <div class="btm-nav">
 
   <button class="text-primary active">
-    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-xl" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
+    <font-awesome-icon class="h-5 w-5 text-xl" :icon="['fas', 'house']" />
     <span class="btm-nav-label">Home</span>
   </button>
   <button class="text-primary active">
@@ -28,8 +28,8 @@
     <span class="btm-n
     av-label">Menu</span>
   </button> -->
-  <button  class="text-primary"> 
-    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-xl" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+  <button  class="text-primary">
+    <font-awesome-icon class="h-5 w-5 text-xl" :icon="['fas', 'magnifying-glass']" />
     <span class="btm-nav-label">Search</span>
   </button>
 </div>
@@ -40,4 +40,8 @@
 export default {};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.btm-nav-labe{
+  font-size: 1.5rem;
+}
+</style>
